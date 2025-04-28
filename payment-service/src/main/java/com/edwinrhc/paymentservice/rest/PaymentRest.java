a0d1cedd772f9d4d39c7a522d38bb8c08a33a2fa
Edwin HC <edwinrhc@gmail.com>
2025-04-28 17:10:04 -0500

    feat(Payment): Se agrego los endpoints Post,Get,Put

---
+++ payment-service/src/main/java/com/edwinrhc/paymentservice/rest/PaymentRest.java
@@ -0,0 +1,29 @@
+package com.edwinrhc.paymentservice.rest;
+
+import com.edwinrhc.paymentservice.dto.payment.CreatePaymentDTO;
+import com.edwinrhc.paymentservice.dto.payment.UpdatePaymentDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/payments")
+public interface PaymentRest {
+
+    @PostMapping(value="/add")
+    ResponseEntity<String> createPayment(@RequestBody @Valid CreatePaymentDTO dto);
+
+    @PutMapping(value="/update")
+    ResponseEntity<String> updatePayment(@RequestBody @Valid UpdatePaymentDTO dto);
+
+    @GetMapping(value="/get")
+    ResponseEntity<List<CreatePaymentDTO>> getAllPayments();
+
+    @GetMapping(value="/{id}")
+    ResponseEntity<List<CreatePaymentDTO>> getPaymentById(@PathVariable Long id);
+
+    @GetMapping(value="/status/{status}")
+    ResponseEntity<List<CreatePaymentDTO>> getPaymentByStatus(@PathVariable String status);
+
+}
