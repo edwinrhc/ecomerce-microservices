b2b46375c02029394f2556f5a7a0b1ba789405db
Edwin HC <edwinrhc@gmail.com>
2025-04-28 14:30:30 -0500

    feat(Payment): se agrego el metodo package GlobalExceptionHandler
    - GlobalExceptionHandler
    - ResourceNotFoundException

---
+++ payment-service/src/main/java/com/edwinrhc/paymentservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.edwinrhc.paymentservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String,String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach( error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String,String>> handleResourceNotFound(ResourceNotFoundException ex){
+        Map<String,String> error = new HashMap<>();
+        error.put("message", ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String,String>> handleRuntime(RuntimeException ex){
+        Map<String,String> error = new HashMap<>();
+        error.put("error","Error interno del servidor: " + ex.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
