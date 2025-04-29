260da2d689634d7ed03ea1ca89681d85f22b053e
Edwin HC <edwinrhc@gmail.com>
2025-04-29 18:21:23 -0500

    feat(AuthService): Creacion del servicio Auth-Service

---
+++ auth-service/src/main/java/com/edwinrhc/authservice/service/AuthService.java
@@ -0,0 +1,17 @@
+package com.edwinrhc.authservice.service;
+
+
+import com.edwinrhc.authservice.dto.user.CreateUserDTO;
+import com.edwinrhc.authservice.dto.user.UpdateUserDTO;
+import org.springframework.http.ResponseEntity;
+
+public interface AuthService {
+
+    ResponseEntity<String> createUser(CreateUserDTO createUserDTO);
+    ResponseEntity<String> updateUser(UpdateUserDTO updateUserDTO);
+    ResponseEntity<String> deleteUser(String userId);
+
+
+
+
+}
