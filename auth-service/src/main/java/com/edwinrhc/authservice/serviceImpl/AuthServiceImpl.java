f0f3ea02f005f3fe3e5bbaed81d48fbc01e9332d
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-05-02 23:43:46 -0500

    feat: Implementando contraseñas encriptadas con PasswordEncoder

--- auth-service/src/main/java/com/edwinrhc/authservice/serviceImpl/AuthServiceImpl.java
+++ auth-service/src/main/java/com/edwinrhc/authservice/serviceImpl/AuthServiceImpl.java
@@ -12,6 +12,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -24,11 +25,16 @@ public class AuthServiceImpl implements AuthService {
     @Autowired
     ModelMapper modelMapper;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public ResponseEntity<String> createUser(CreateUserDTO createUserDTO) {
         log.info("createUser : {}", createUserDTO);
         try{
             User user = modelMapper.map(createUserDTO, User.class);
+
+            user.setPassword(passwordEncoder.encode(createUserDTO.getPassword()));
             User savedUser = userRepository.save(user);
             CreateUserDTO result = modelMapper.map(savedUser, CreateUserDTO.class);
             return AuthUtils.getResponseEntity(AuthConstants.SUCCESS, HttpStatus.OK);
