90d592a8b20aefcc1d7489898c15d4b065b8e6a9
Edwin Huamanttupa <edwinrhc@gmail.com>
2025-05-03 23:39:14 -0500

    feat(Auth-Service): Se creo el end point GET getAllUsers

--- auth-service/src/main/java/com/edwinrhc/authservice/rest/AuthRest.java
+++ auth-service/src/main/java/com/edwinrhc/authservice/rest/AuthRest.java
@@ -3,11 +3,15 @@ package com.edwinrhc.authservice.rest;
 
 import com.edwinrhc.authservice.dto.user.CreateUserDTO;
 import com.edwinrhc.authservice.dto.user.LoginDTO;
+import com.edwinrhc.authservice.dto.user.UserListDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/api/user")
 public interface AuthRest {
@@ -17,6 +21,9 @@ public interface AuthRest {
 
     @PostMapping(path="/login")
     ResponseEntity<String> login(@RequestBody @Valid LoginDTO loginDTO);
+
+    @GetMapping("/get")
+    ResponseEntity<List<UserListDTO>> getAllUser();
 
 
 }
--- auth-service/src/main/java/com/edwinrhc/authservice/restImpl/AuthRestImpl.java
+++ auth-service/src/main/java/com/edwinrhc/authservice/restImpl/AuthRestImpl.java
@@ -3,6 +3,7 @@ package com.edwinrhc.authservice.restImpl;
 import com.edwinrhc.authservice.constants.AuthConstants;
 import com.edwinrhc.authservice.dto.user.CreateUserDTO;
 import com.edwinrhc.authservice.dto.user.LoginDTO;
+import com.edwinrhc.authservice.dto.user.UserListDTO;
 import com.edwinrhc.authservice.rest.AuthRest;
 import com.edwinrhc.authservice.service.AuthService;
 import com.edwinrhc.authservice.utils.AuthUtils;
@@ -10,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class AuthRestImpl implements AuthRest {
@@ -35,5 +39,15 @@ public class AuthRestImpl implements AuthRest {
             ex.printStackTrace();
         }
         return AuthUtils.getResponseEntity(AuthConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<List<UserListDTO>> getAllUser() {
+        try{
+            return authService.listUsers();
+        }catch(Exception ex){
+            ex.printStackTrace();
+            return new ResponseEntity<List<UserListDTO>>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
